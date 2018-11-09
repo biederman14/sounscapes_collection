be9f497a40169f9d2031234303cc448367c0d647
Ingrid Biederman <biederman.14@gmail.com>
2018-11-09 14:54:05 -0500

    updated homeWrapper to add export and fixed typos

---
+++ src/main/resources/static/js/homeWrapper.js
@@ -0,0 +1,10 @@
+const HomeWrapper = (function() {
+	const homeWrapper = document.createElement('div')
+	homeWrapper.classList.add('wrapper')
+
+	return homeWrapper
+})()
+
+module.exports = {
+	HomeWrapper
+}
