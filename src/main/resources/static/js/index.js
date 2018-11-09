ce873613c120b4b71ce28be8d0f5538acdff6522
Ingrid Biederman <biederman.14@gmail.com>
2018-11-09 12:04:03 -0500

    index.js partially right. information isn't displayed, but console displays info.

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,15 +1,60 @@
 
-const {HomeWrapper} = require('./homeWrapper')
+const {HomeWrapper} = require('./home\Wrapper')
 const {header} = require('.header')
 
 
+//Imported Components
 
+
+
+//App Components
+
+
+
+//Imported Tools
+
+
+
+
+//Build App
+
+
+
+
+//Boostrap Application
 
 
 
 
 //Artists
+const getArtists = () => {
 
+	fetch('/api/artists')
+	.then((res)=>res.json())
+	.then((data)=>{
+		let output='<button class ="btn">Artist<button>';
+		data.forEach(function(artist){
+			output += `
+			<ul class="artistList">
+				<li class="artistItem">Artist Name: ${artist.artistName}</li>
+				<li class="artistImage">Artist Image: ${artist.artistImage}</li>
+				<li class="artistHometown">Artist Home Town: ${artist.artistHometown}</li>
+			</ul>
+			`
+		})
+	})}
+
+document.querySelector('getArtists').addEventListener('click', getArtists);
+
+const artist = {
+	artistName: 
+	artistImage:
+	artistHometown:
+}
+
+module.export = {
+	getArtists()
+	}
 
 //Albums
 
